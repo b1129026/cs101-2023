c380c32550d433d10e3db009a0bc2d3c49d811ca
b1129026 <119938471+b1129026@users.noreply.github.com>
2023-05-11 14:43:06 +0800

    Add files via upload

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,99 +1,121 @@
-#include<iostream>
-#include<string>
-
+#include <iostream>
 using namespace std;
-class Engine {
-    private:
-        int m_EngineCapacity;
-        int m_Horsepower;
-        bool m_EngineRunning;
-    public:
-        Engine(int EngineCapacity=2000, int Horsepower = 200){
-            m_EngineCapacity = EngineCapacity;
-            m_Horsepower = Horsepower;
-            m_EngineRunning = false;
+
+class Fueltank{
+private:
+    int m_FueltankCapacity;
+    int m_Gas_Grade;
+    
+public:
+    Fueltank(int FuelTankCapacity = 3000, int Gas = 98){
+        m_FueltankCapacity = FuelTankCapacity;
+        m_Gas_Grade = Gas;
+    }
+    
+    int fuel_up(int v, int gas){
+        if(v > m_FueltankCapacity){
+            cout<<"Error: FuelTankCapacity : "<<m_FueltankCapacity<<" but fuel up : "<<v<<endl;
         }
-    bool EngineStart(){
-        m_EngineRunning = true;
-        return true;
+        if(gas != m_Gas_Grade){
+            cout<<"Error: Gas_grade: "<<gas<<" Correct Gas_Grade: "<<m_Gas_Grade<<endl;
+        }
+        if(v == m_FueltankCapacity && gas == m_Gas_Grade){
+            cout<<"fuel_up: "<<v<<" Gas_grade: "<<gas<<endl;
+        }
+        return v;
     }
-    bool EngineStop(){
-        m_EngineRunning = false;
-        return false;
+    
+    int set_Gas_grade(int Gas_grade){
+        m_Gas_Grade = Gas_grade;
+        return m_Gas_Grade;
     }
-    bool get_EngineStatus(){
-        return m_EngineRunning;
+    
+    int get_Gas_grade(){
+        return m_Gas_Grade;
     }
 };
 
-class Fueltank {
-    private:
-        int m_FueltankCapacity;
-        int m_Gas_grade;
-    public:
-        Fueltank(int FueltankCapacity=3000, int Gas=98){
-            m_FueltankCapacity = FueltankCapacity;
-            m_Gas_grade = Gas;
-        }
-        int fuel_up(int v, int gas){
-            if(v > m_FueltankCapacity){
-                cout << "Error: FueltankCapacity: " << m_FueltankCapacity <<" but fuel up: " << v << endl;
-            }
-            if(gas != m_Gas_grade){
-                cout<<"Error: Gas_grade: " << gas << " Correct Gas_grade: " << m_Gas_grade << endl;
-            }
-            if(v <= m_FueltankCapacity && gas == m_Gas_grade){
-                cout << "Fuel_up: " << v << " Gas_grade: " << gas << endl;
-            }
-            return 0;
-        }   
-        int set_Gas_grade(int Gas_grade){
-            m_Gas_grade = Gas_grade;
-            return 0;
-        }
-        int get_Gas_grade(){
-            cout<< m_Gas_grade << endl;
-            return 0;
-        }
-
+class Car{
+public:
+    Fueltank m_Fueltank;
+    int m_Maxseating;
+    int price;
+    int base;
+    string m_brand;
+    string m_model;
+    int m_year;
+    
+public:
+    Car(string x, string y, int z, int s){
+        m_brand = x;
+        m_model = y;
+        m_year = z;
+        m_Maxseating = s;
+    }
+    int get_Gas_grade(){
+       return m_Fueltank.get_Gas_grade(); 
+    }
+    int set_Gas_grade(int gas = 98){
+        cout<<"Set_Gas_grade : "<<gas<<endl;
+        return m_Fueltank.set_Gas_grade(gas);
+    }
+    int fuel_up(int v, int gas = 98){
+        return m_Fueltank.fuel_up(v, gas);
+    }
 };
 
-class Car {
+
+class BMW_Car : public Car{
     private:
-        Engine m_Engine;
-        Fueltank m_Fueltank;
-        int m_MaxSeating;
-        int m_price;
-        int m_base;
-        string m_brand;
-        string m_model;
-        int m_year;
-        void m_UpdatePrice(int base=500000){
-            
-        }
+    string m_DriveMode;
+    
     public:
-        Car(string x,  int z, int s){
-            m_model = x;
-            m_year = z;
-            m_MaxSeating = s;
-        }
+    BMW_Car(string y, int z, int s) : Car("BMW", y, z, s){
+        cout<<"Constructing BMW_Car\n";
+        m_DriveMode = "Rear-Wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
 };
 
-class Audi_Car : public Car{
+class AUDI_Car : public Car{
     private:
-        
-
+    string m_DriveMode;
+    
     public:
-        Engine m_Engine;
-        Fueltank m_Fueltank;
-        Audi_Car(string x, int z, int s) : Car(x,z,s){
-            cout << "Constructing Audi_Car" << endl;
-            }
-        
+    AUDI_Car(string y, int z, int s) : Car("AUDI", y, z, s){
+        cout<<"Constructing AUDI_Car\n";
+        m_DriveMode = "Front-Wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+    string get_brand(){
+        return m_brand;
+    }
+};
 
+class BENZ_Car : public Car{
+    private:
+    string m_DriveMode;
+    
+    public:
+    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s){
+        cout<<"Constructing BENZ_Car\n";
+        m_DriveMode = "Front-Wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
 };
 
 int main(){
-    Audi_Car car_a("A1", 2021, 5);
-    car_a.m_Fueltank.fuel_up(3000,98);
+    AUDI_Car car_2 ("A1", 2021, 2);
+    cout<<car_2.get_brand()<<" : Gas_grade = "<<car_2.get_Gas_grade()<<endl;
+    car_2.set_Gas_grade(95);
+    cout<<car_2.get_brand()<<" : Gas_Grade = "<<car_2.get_Gas_grade()<<endl;
+    car_2.fuel_up(3000, 95);
+    
+    return 0;
 }
