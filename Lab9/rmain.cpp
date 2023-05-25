0babcf052eb020f9a2108833b8fac1a04b017fb6
b1129026 <119938471+b1129026@users.noreply.github.com>
2023-05-25 14:29:12 +0800

    Add files via upload

---
+++ Lab9/rmain.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    public:
+    ReplaceMyString() {}
+    void replaceString(string needle, string stick) {
+        ifstream in("main.cpp");
+        ofstream out("rmain.cpp");
+        char c;
+        while (true) {
+            
+            in.get(c);
+            if (in.eof()) {
+                break;
+            }
+            
+            if (c == needle[0]) {
+                string buff="";
+                buff += c;
+                for (int i=1; i<needle.length(); i++) {
+                    
+                    in.get(c);
+                    buff += c;
+                    if (c != needle[i]) {
+                        out << buff;
+                        break;
+                    }
+                    if (i == needle.length()-1) {
+                        for (int j=0; j<stick.length(); j++) {
+                            out << stick[j];
+                        }
+                    }
+                }
+            } else {
+                out << c;
+            }
+        }
+        in.close();
+        out.close();
+    }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU is best", "IU is best is best");
+    return 0;
+}
